e2c052d8dfa5425f1dfccd9cf74fc4d044f7a831
NCP08 <navercloudpaltform08.gmail.com>
2024-01-24 17:25:29 +0900

    자료형 기본형

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,27 @@
+package ch02.variable01;
+
+public class DataType {
+
+	public static void main(String[] args) {
+			int num = 100; 		// 정수
+			double dnum = 3.14; // 실수
+			char ch = 'a';		// 문자 하나
+			String name = "홍길동"; // 문자열
+			boolean isRun = true; // true, false
+			String str = "a";		  // 문자열인데 문자가 하나
+			String name1 = "Albert";
+			
+			// 자료형 변수 공간 이름 = 값
+			System.out.println(num);
+			num = 999;				// num에 999를 저장
+			System.out.println(num);
+			System.out.println(dnum);
+			System.out.println(ch);
+			System.out.println(name);
+			System.out.println(isRun);
+			System.out.println(str);
+			System.out.println(name1);
+			
+	}
+
+}
