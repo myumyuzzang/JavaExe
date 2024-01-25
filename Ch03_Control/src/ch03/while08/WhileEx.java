e22283fffc9e08913c3be8df7752dc9baebb4997
NCP08 <navercloudpaltform08.gmail.com>
2024-01-25 17:15:27 +0900

    for문 예제

--- Ch03_Control/src/ch03/while08/WhileEx.java
+++ Ch03_Control/src/ch03/while08/WhileEx.java
@@ -5,10 +5,10 @@ public class WhileEx {
 	public static void main(String[] args) {
 		// while : ()이 true인 동안 계속 반복하라
 		
-		int i=3;	// 초기식 (시작:언제부터)
-		while(i<10) {	// 조건 (종료:언제까지)
-			i++;	// 증감식 (변화:얼마만큼)
+		int i=1;	// 초기식 (시작:언제부터)
+		while(i<=10) {	// 조건 (종료:언제까지)
 			System.out.print(i + ", "); // 실행문
+			i++;	// 증감식 (변화:얼마만큼)
 		}
  
 	}
