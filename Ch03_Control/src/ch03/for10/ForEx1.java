049df83e85e567d537b2d4d95ebc9bd7640b8c74
NCP08 <navercloudpaltform08.gmail.com>
2024-01-25 17:21:20 +0900

    for문 예제

---
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -0,0 +1,16 @@
+package ch03.for10;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		// 1~10까지 출력
+		
+		// 1번만 실행;조건(계속비교);실행이 끝나면 변화
+		// 시작;끝;변화
+		for(int i=1;i<=10;i++) {				
+			System.out.println(i);	
+							
+		}
+
+	}
+}
