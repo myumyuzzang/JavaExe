9ba8c3a96f021d715bb7f0bc78f5f48d6348f1cf
NCP08 <navercloudpaltform08.gmail.com>
2024-01-26 16:59:35 +0900

    교재 188페이지 예문 배열 복사

---
+++ Ch05_Array/src/ch05/array05/ch05/sec09/ArrayCopyExample.java
@@ -0,0 +1,17 @@
+package ch05.array05.ch05.sec09;
+
+public class ArrayCopyExample {
+	// 교재 187페이지 예문 배열 복사
+	public static void main(String[] args) {
+		// 길이가 3인 배열
+		String[] oldStrArray = { "java", "array", "copy" };
+		// 길이가 5인 배열을 새로 생성
+		String[] newStrArray = new String[5];
+		// 배열 항목 복사
+		System.arraycopy(oldStrArray, 0, newStrArray, 0, oldStrArray.length);
+		// 배열 항목 출력
+		for(int i=0; i<newStrArray.length; i++) {
+			System.out.print(newStrArray[i] + ", ");
+		}
+	}
+}
