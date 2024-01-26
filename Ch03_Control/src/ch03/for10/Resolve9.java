d4df0d59dd46918cb73fdbe9c2ccdbec312c3974
NCP08 <navercloudpaltform08.gmail.com>
2024-01-26 10:53:40 +0900

    resolve9 풀이

---
+++ Ch03_Control/src/ch03/for10/Resolve9.java
@@ -0,0 +1,16 @@
+package ch03.for10;
+
+public class Resolve9 {
+
+	public static void main(String[] args) {
+		// 9. for문을 이용해서 10부터 20까지 출력하세요
+		
+		// 1번만 실행;조건(계속비교);실행이 끝나면 변화
+		// 시작;끝;변화
+		for(int i=10;i<=20;i++) {				
+			System.out.println(i);	
+							
+		}
+
+	}
+}
