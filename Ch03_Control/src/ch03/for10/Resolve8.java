aeab7931d08ca4e70a8d2eb7347b9edb7af15ead
NCP08 <navercloudpaltform08.gmail.com>
2024-01-26 10:53:13 +0900

    resolve8 풀이

---
+++ Ch03_Control/src/ch03/for10/Resolve8.java
@@ -0,0 +1,16 @@
+package ch03.for10;
+
+public class Resolve8 {
+
+	public static void main(String[] args) {
+		// 8. for문을 이용해서 1부터 100까지 출력하세요
+		
+		// 1번만 실행;조건(계속비교);실행이 끝나면 변화
+		// 시작;끝;변화
+		for(int i=1;i<=100;i++) {				
+			System.out.println(i);	
+							
+		}
+
+	}
+}
