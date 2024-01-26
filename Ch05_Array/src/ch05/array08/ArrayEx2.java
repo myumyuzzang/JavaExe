f2d38c924ca3eb000daba13473993681287fd059
NCP08 <navercloudpaltform08.gmail.com>
2024-01-26 17:38:33 +0900

    이차원 배열 예제 추가

---
+++ Ch05_Array/src/ch05/array08/ArrayEx2.java
@@ -0,0 +1,26 @@
+package ch05.array08;
+
+import java.util.Arrays;
+
+public class ArrayEx2 {
+
+	public static void main(String[] args) {
+		// 이차원 배열
+		// arr은 1차원 배열을 가르키는 3개의 참조변수 배열을 가리킨다.(참조값을 가진다)
+		int[][] arr = new int[3][];
+		// arr의 배열 요소는 1차원 배열을 가리킨다. (참조값을 가진다)
+		arr[0] = new int[3];
+		arr[1] = new int[4];
+		arr[2] = new int[5];
+		
+		for(int i = 0; i <arr.length;i++) {
+			for(int j = 0; j<arr[i].length; j++) {
+				arr[i][j] = i*3 + j;
+			}
+		}
+		System.out.println(arr);
+		System.out.println(Arrays.toString(arr));
+		for(int[] temp : arr)
+		System.out.println(Arrays.toString(temp));
+	}
+}
