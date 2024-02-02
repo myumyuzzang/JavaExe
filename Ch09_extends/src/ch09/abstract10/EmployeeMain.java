8d696e3b4e14103151ec82f0e69361e4b5492a05
NCP08 <navercloudpaltform08.gmail.com>
2024-02-02 10:16:11 +0900

    프로그램 구현 추가

--- Ch09_extends/src/ch09/abstract10/EmployeeMain.java
+++ Ch09_extends/src/ch09/abstract10/EmployeeMain.java
@@ -2,7 +2,8 @@ package ch09.abstract10;
 
 public class EmployeeMain {
 	public static void main(String[] args) {
-		
+		EmployeeManager em = new EmployeeManager();
+		em.run();
 	}
 
 }
--- Ch09_extends/src/ch09/abstract10/EmployeeManager.java
+++ Ch09_extends/src/ch09/abstract10/EmployeeManager.java
@@ -10,7 +10,15 @@ public class EmployeeManager {
 		private Scanner sc = new Scanner(System.in);
 		
 		private int viewMenu() {
-			return 0;
+			System.out.println("[ 사원 선택 ]");
+			System.out.println("1. 정규직");
+			System.out.println("2. 임시직");
+			System.out.println("3. 일용직");
+			System.out.println("4. 전체 정보 보기");
+			System.out.println("5. 종료");
+			System.out.println("번호 입력 >> ");
+			int sel = sc.nextInt();			
+			return sel;
 			
 		}
 		private RegularEmployee createRegularEmployee() {
@@ -29,6 +37,25 @@ public class EmployeeManager {
 			
 		}
 		public void run() {
-			
+			boolean isRun = true;
+			while(isRun) {
+				int selNum = viewMenu();
+				switch(selNum) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					isRun = false;
+					break;
+				default:
+					break;
+				}
+			}		
+			System.out.println("Program Exit");
 		}
 	}
